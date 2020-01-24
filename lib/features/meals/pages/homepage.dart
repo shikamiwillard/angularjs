9d9841c13e29929c4cd79e032a22386f37576bb0
Willard Shikami <willardshikami@gmail.com>
2020-01-24 15:32:28 +0300

    ui: generate items for listview placeholder

--- lib/features/meals/pages/homepage.dart
+++ lib/features/meals/pages/homepage.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        
-      ),
+      body: getOurMeals(),
     );
   }
-  
+
+  List<String> ourMeals() {
+    var meals = List<String>.generate(20, (counter) => 'Item $counter');
+    return meals;
+  }
+
+  Widget getOurMeals() {
+    var allMeals = ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+
+        );
+      },
+    );
+
+    return allMeals;
+  }
 }
--- lib/store/reducers/reducer.dart
+++ lib/store/reducers/reducer.dart
@@ -1,27 +1,27 @@
-import 'package:mealtime/store/models/meal_model.dart';
-import 'package:mealtime/store/actions/actions.dart';
-import 'package:mealtime/store/store.dart';
+// import 'package:mealtime/store/models/meal_model.dart';
+// import 'package:mealtime/store/actions/actions.dart';
+// import 'package:mealtime/store/store.dart';
 
-AppState appReducer(AppState state, action) {
-  return AppState(
-    // mealmodel: mealReducer(state.mealmodel, action)
-    mealmodel: mealReducer(state.mealmodel, action)
-  );
+// // AppState appReducer(AppState state, action) {
+// //   return AppState(
+// //     // mealmodel: mealReducer(state.mealmodel, action)
+// //     mealmodel: mealReducer(state.mealmodel, action)
+// //   );
 
-  List<MealModel> mealReducer(List<MealModel> state, action){
-    if(action is AddMealAction){
-      return []
-      ..addAll(state)
-      ..add(MealModel(id: action.id, title: action.title));
-    }
+// //   List<MealModel> mealReducer(List<MealModel> state, action){
+// //     if(action is AddMealAction){
+// //       return []
+// //       ..addAll(state)
+// //       ..add(MealModel(id: action.id, title: action.title));
+// //     }
 
-    if(action is RemoveMealAction){
-      return List.unmodifiable(List.from(state)..remove(action.id));
-    }
+// //     if(action is RemoveMealAction){
+// //       return List.unmodifiable(List.from(state)..remove(action.id));
+// //     }
 
-    if(action is RemoveMealsAction){
-      return List.unmodifiable([]);
-    }
-    return state;
-  }
-}
+// //     if(action is RemoveMealsAction){
+// //       return List.unmodifiable([]);
+// //     }
+// //     return state;
+// //   }
+// // }
